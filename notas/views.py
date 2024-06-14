b158e5d6342b6b44158452a3a7af6fd5039a91fe
Rodrigo Reis <suporteproc@alema.local>
2024-06-14 16:27:10 -0400

    alterando o formulario

--- notas/views.py
+++ notas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Nota
 
@@ -12,8 +12,23 @@ def home(request):
 @login_required(login_url='usuarios:login')
 def cadastrar_nota(request):
     if request.method == 'GET':
-        ufs = Nota.objects.values_list('uf', flat=True)
-        print(ufs)
         return render(request, 'cadastrar_nota.html')
     elif request.method == 'POST':
-        pass
+        
+        nome_razao_social = request.POST.get('nome_razao_social')
+        endereco = request.POST.get('endereco')
+        bairro = request.POST.get('bairro')
+        municipio = request.POST.get('nome_razao_social')
+        cep = request.POST.get('cep')
+        estado = request.POST.get('estado')
+        cnpj_cpf = request.POST.get('cnpj_cpf')
+        iscricao_estadual = request.POST.get('iscricao_estadual')
+        desc_produto = request.POST.get('desc_produto')
+        quantidade = request.POST.get('quantidade')
+        valor_unitario = request.POST.get('nome_razao_social')
+        valor_total = request.POST.get('valor_total')
+
+        
+
+        return HttpResponse(f'{nome_razao_social}')
+        
