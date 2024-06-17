f23e32699e8e1f041414ef4e2f34484bdd7899be
Rodrigo Reis <suporteproc@alema.local>
2024-06-17 13:45:12 -0400

    adicionando o footer e ajeitando a paginacao

--- notas/views.py
+++ notas/views.py
@@ -4,12 +4,43 @@ from .models import Nota
 from .forms import NotaForm
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+
+# @login_required(login_url='usuarios:login')
+# def home(request):
+#     notas = Nota.objects.all()
+#     context = {'notas': notas}
+
+#     paginator = Paginator(notas, 10)  # 10 itens por página
+#     page = request.GET.get('page')
+    
+#     try:
+#         notas = paginator.page(page)
+#     except PageNotAnInteger:
+#         # Se a página não é um número inteiro, exibe a primeira página
+#         notas = paginator.page(1)
+#     except EmptyPage:
+#         # Se a página está fora do intervalo (por exemplo, página 9999), exibe a última página de resultados
+#         notas = paginator.page(paginator.num_pages)
+#     return render(request, 'home.html', context)
 
 @login_required(login_url='usuarios:login')
 def home(request):
     notas = Nota.objects.all()
-    context = {'notas': notas}
+    paginator = Paginator(notas, 10)  # 10 itens por página
+    page = request.GET.get('page')
+
+    try:
+        notas_paginadas = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não é um número inteiro, exibe a primeira página
+        notas_paginadas = paginator.page(1)
+    except EmptyPage:
+        # Se a página está fora do intervalo (por exemplo, página 9999), exibe a última página de resultados
+        notas_paginadas = paginator.page(paginator.num_pages)
+
+    context = {'notas': notas_paginadas}
     return render(request, 'home.html', context)
 
 
